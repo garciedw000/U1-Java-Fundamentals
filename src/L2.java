f3fd00baaf4e717bf38cf05a0d811580bab41515
garciedw000 <garciedw000@warren.k12.in.us>
2021-08-04 11:51:34 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,18 @@
 public class L2 {
 
-    //add the main method header
-}
+    //comments
+        public static void main(String[] args){
+            String clientName = "George Washington";
+            int moneyInAccount = 150000000;
+            int idNumber = 34829348;
+            String bankName = "Chase Bank For Dead Presidents";
+            System.out.println(clientName);
+            System.out.println(moneyInAccount);
+            System.out.println(idNumber);
+            System.out.println(bankName);
+        }
+
+    }
+
+
+
